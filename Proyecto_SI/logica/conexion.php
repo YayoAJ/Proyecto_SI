05502030115fc393879cc5af50e8f9a75425cfd5
YayoAJ <124760353+YayoAJ@users.noreply.github.com>
2024-05-06 22:39:49 -0600

    Add files via upload

--- Proyecto_SI/logica/conexion.php
+++ Proyecto_SI/logica/conexion.php
@@ -1,12 +1,10 @@
 <?php
-$host="127.0.0.1";
-$port="3306";
-$socket="";
-$user="root";
-$password="Marino21";
-$dbname="db";
+$host="sql113.infinityfree.com";
+$user="if0_36499435";
+$password="9mHixvYjkY3";
+$dbname="if0_36499435_tercera_capa";
 
-$conexion = new mysqli($host, $user, $password, $dbname, $port, $socket);
+$conexion = new mysqli($host, $user, $password, $dbname);
 
 if($conexion->connect_error){
     echo "no hay conexion";
